3d64eea73f0776d022b2297cbc92bbc40794e712
walidcse <ibrahimwalidkhan0@gmail.com>
2025-10-03 21:53:00 +0600

    fea. new

--- Beecrowd.1001.c
+++ Beecrowd.1001.c
@@ -3,11 +3,12 @@
 
 int main()
 {
-    int A, B , sum;
+    int A, B , sum,min;
     scanf("%d %d ",&A,&B);
     sum = A + B;
+    min = A-B;
     printf("X = %d\n",sum);
-
+    printf("X = %d\n",min);
 
     return 0;
 }
